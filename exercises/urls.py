747ed86775c3de774439f26352f366984db65f22
GabrielG12 <65672728+GabrielG12@users.noreply.github.com>
2023-05-08 15:57:34 +0200

    Class-based API VIEWS 1

--- exercises/urls.py
+++ exercises/urls.py
@@ -3,8 +3,10 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.list_exercises, name="exercises_list"),
-    path('<int:exercise_id>/', views.exercise_detail, name="exercise_detail"),
-    path('update/<int:exercise_id>/', views.exercise_update, name="exercise_update"),
-    path('delete/<int:exercise_id>/', views.exercise_delete, name="exercise_delete"),
+
+    #CLASS_BASED
+    path('', views.ExerciseListCreateView.as_view(), name="exercises_list_create"),
+    path('<int:exercise_id>/', views.ExerciseRetrieveUpdateDeleteView.as_view(), name="exercises_retrieve_update_delete"),
+
+
 ]
--- exercises/views.py
+++ exercises/views.py
@@ -2,48 +2,50 @@ from django.shortcuts import render
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, APIView
 from .models import Exercises
 from .serializers import ExercisesSerializer
 from django.shortcuts import get_object_or_404
 
 
-@api_view(http_method_names=["GET", "POST"])
-def list_exercises(request:Request):
+#TODO: VIEW FOR LISTING AND CREATING AN EXERCISE
+class ExerciseListCreateView(APIView):
 
-    exercises = Exercises.objects.all()
-    if request.method == "POST":
+    serializer_class = ExercisesSerializer
+    def get(self, request, *args, **kwargs):
+
+        exercises = Exercises.objects.all()
+        serializer = ExercisesSerializer(instance=exercises, many=True)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, *args, **kwargs):
+
         data = request.data
-        serializer = ExercisesSerializer(data=data)
+        serializer = self.serializer_class(data=data)
         if serializer.is_valid():
             serializer.save()
             return Response(data=serializer.data, status=status.HTTP_201_CREATED)
         return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    serializer = ExercisesSerializer(instance=exercises, many=True)
-    return Response(data=serializer.data, status=status.HTTP_200_OK)
 
-@api_view(http_method_names=["GET"])
-def exercise_detail(request:Request, exercise_id:int):
+class ExerciseRetrieveUpdateDeleteView(APIView):
 
-    exercise = get_object_or_404(Exercises, pk=exercise_id)
-    serializer = ExercisesSerializer(instance=exercise)
-    return Response(data=serializer.data, status=status.HTTP_200_OK)
+    serializer_class = ExercisesSerializer
 
-@api_view(http_method_names=["PUT"])
-def exercise_update(request:Request, exercise_id:int):
-
-    exercise = get_object_or_404(Exercises, pk=exercise_id)
-    data = request.data
-    serializer = ExercisesSerializer(instance=exercise, data=data)
-    if serializer.is_valid():
-        serializer.save()
+    def get(self, request, exercise_id:int):
+        exercise = get_object_or_404(Exercises, pk=exercise_id)
+        serializer = self.serializer_class(instance=exercise)
         return Response(data=serializer.data, status=status.HTTP_200_OK)
-    return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(http_method_names=["DELETE"])
-def exercise_delete(request:Request, exercise_id:int):
+    def put(self, request, exercise_id:int):
+        exercise = get_object_or_404(Exercises, pk=exercise_id)
+        data = request.data
+        serializer = self.serializer_class(instance=exercise, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data=serializer.data, status=status.HTTP_200_OK)
+        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    exercise = get_object_or_404(Exercises, pk=exercise_id)
-    exercise.delete()
-    return Response(status=status.HTTP_204_NO_CONTENT)
-
+    def delete(self, request, exercise_id:int):
+        exercise = get_object_or_404(Exercises, pk=exercise_id)
+        exercise.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
